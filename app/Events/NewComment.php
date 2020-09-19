8a1d93719129e5b13a5d743749de4d1b9c2d27dd
Muhsin Zyne <muhsin.zyne@gmail.com>
2020-09-19 17:07:22 +0300

    inital commit

---
+++ app/Events/NewComment.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use App\Comment;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NewComment implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $comment;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('posts.' . $this->comment->post->id);
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'body' => $this->comment->body,
+            'created_at' => $this->comment->created_at->toFormattedDateString(),
+            'user' => [
+                'name' => $this->comment->user->name,
+                'avatar' => 'http://placeimg.com/80/80'
+            ],
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('posts/{post}/comments', 'CommentController@index');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('posts/{post}/comment', 'CommentController@store');
+});
